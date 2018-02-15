889cee94822a4f1b8530eb931d1e4d85d9d8349f
DanielPLeal <danielpleal@gmail.com>
2018-02-15 14:50:32 -0200

    Atualização

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -17,14 +17,14 @@ class Page {
 		$this->options = array_merge($this->defaults, $opts);
 
 		$config = array(
-			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
-			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."views-cache/",
+			"tpl_dir"       => $_SERVER['DOCUMENT_ROOT']."/views/",
+			"cache_dir"     => $_SERVER['DOCUMENT_ROOT']."/views-cache/",
 			"debug"         => false 
 	    );
 
 		Tpl::configure( $config );
 
-		$this->tpl = new Tpl();
+		$this->tpl = new Tpl;
 
 		$this->setData($this->options["data"]);
 		$this->tpl->draw("header");
@@ -39,13 +39,12 @@ class Page {
 
 	public function setTpl($name, $data = array(), $returnHTML = false)
 	{
-		$this->setData();
+		$this->setData($data);
 		return  $this->tpl->draw($name, $returnHTML);
 	}
 
-	public function __destruct()
-	{
-
+	public function __destruct(){
+		$this->tpl->draw("footer");
 	}
 
 }
